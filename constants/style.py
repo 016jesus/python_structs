aaf45b18e18a1264221b38bdfb06a868c750725d
Jesus <113791040+016jesus@users.noreply.github.com>
2024-05-25 11:42:12 -0500

    Update style.py

--- constants/style.py
+++ constants/style.py
@@ -1,31 +1,41 @@
 
 #estilos definidos al programa
 windowSize = "1300x800"
-backgroundColor = "#0A1B0E"
+backgroundColor = "#80c9b8" # gris medio oscurito jeje xd
+main = "#98e1d0"
+
 node_color = 'w'
-edge_color = "#7ED130"
+edge_color = "black" #verde clarito
 titulo = "Proyecto Estructuras de datos"
 file = "grafo_villavo.pkl"
 font = "Bahnschrift"
-verdelimaOscuro = "#172907"
+verdelimaOscuro = "#172907" 
 negroBotones = "#1F1F1A"
 
-def style_unvisited_edge(G, edge):        
+
+
+
+
+
+
+
+def unvisited_edge(G, edge):        
     G.edges[edge]["color"] = "#d36206"
     G.edges[edge]["alpha"] = 0.2
     G.edges[edge]["linewidth"] = 0.5
 
-def style_visited_edge(G, edge):
+def visited_edge(G, edge):
     G.edges[edge]["color"] = "#d36206"
     G.edges[edge]["alpha"] = 1
     G.edges[edge]["linewidth"] = 1
 
-def style_active_edge(G, edge):
+def active_edge(G, edge):
     G.edges[edge]["color"] = '#e8a900'
     G.edges[edge]["alpha"] = 1
     G.edges[edge]["linewidth"] = 1
 
-def style_path_edge(G, edge):
+def path_edge(G, edge):
     G.edges[edge]["color"] = "white"
     G.edges[edge]["alpha"] = 1
+    G.edges[edge]["linewidth"] = 1] = 1
     G.edges[edge]["linewidth"] = 1
